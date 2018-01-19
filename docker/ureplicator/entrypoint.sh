240a6293e1c1666ada585cdee6db2b98079ad56a
Daniel Coman <daniel.alex.coman@gmail.com>
2018-01-19 19:03:32 +0200

    ureplicator image new version

--- docker/ureplicator/entrypoint.sh
+++ docker/ureplicator/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-cd /uReplicator/uReplicator-Distribution/target/uReplicator-Distribution-pkg/bin
+cd /uReplicator/uReplicator-Distribution/target/uReplicator-Distribution-pkg/bin/
 
-exec "./${SERVICE_TYPE}"
+if [ ${SERVICE_TYPE} == "controller" ] ; then
+./start-controller.sh \
+          -port 9000 \
+          -zookeeper zookeeper.ureplicator.svc.cluster.local:2181 \
+          -helixClusterName testCluster \
+          -backUpToGit false \
+          -autoRebalanceDelayInSeconds 120 \
+          -localBackupFilePath /tmp/uReplicator-controller \
+          -enableAutoWhitelist true \
+          -enableAutoTopicExpansion true \
+          -srcKafkaZkPath zookeeper.kafka-source.svc.cluster.local:2181 \
+          -destKafkaZkPath zookeeper.kafka-destination.svc.cluster.local:2181
+
+elif [ ${SERVICE_TYPE} == "worker" ] ; then
+./start-worker.sh \
+          -consumer.config /uReplicator/config/consumer.properties \
+          -producer.config /uReplicator/config/producer.properties \
+          -helix.config /uReplicator/config/helix.properties
+fi
