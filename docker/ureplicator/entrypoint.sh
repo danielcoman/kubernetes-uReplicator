95d62b8e010d10a8e8263601d6b212191e1429b3
Daniel Coman <daniel.alex.coman@gmail.com>
2018-02-20 15:14:34 +0200

    Updated image

--- docker/ureplicator/entrypoint.sh
+++ docker/ureplicator/entrypoint.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-cd /uReplicator/uReplicator-Distribution/target/uReplicator-Distribution-pkg/bin/
+cd /uReplicator/bin/
 
 if [ ${SERVICE_TYPE} == "controller" ] ; then
 ./start-controller.sh \
           -port 9000 \
           -zookeeper zookeeper.ureplicator.svc.cluster.local:2181 \
-          -helixClusterName testCluster \
+          -helixClusterName testMirrorMaker \
           -backUpToGit false \
           -autoRebalanceDelayInSeconds 120 \
           -localBackupFilePath /tmp/uReplicator-controller \
           -enableAutoWhitelist true \
           -enableAutoTopicExpansion true \
           -srcKafkaZkPath zookeeper.kafka-source.svc.cluster.local:2181 \
-          -destKafkaZkPath zookeeper.kafka-destination.svc.cluster.local:2181
+          -destKafkaZkPath zookeeper.kafka-destination.svc.cluster.local:2181 \
+          -initWaitTimeInSeconds 10 \
+          -refreshTimeInSeconds 20
 
 elif [ ${SERVICE_TYPE} == "worker" ] ; then
 ./start-worker.sh \
-          -consumer.config /uReplicator/config/consumer.properties \
-          -producer.config /uReplicator/config/producer.properties \
-          -helix.config /uReplicator/config/helix.properties
+          --helix.config /uReplicator/config/helix.properties \
+          --consumer.config /uReplicator/config/consumer.properties \
+          --producer.config /uReplicator/config/producer.properties
 fi
